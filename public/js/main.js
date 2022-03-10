29cbdc1c750c384ce9638310c875898b239d7340
Pushkar1199 <pushkar480@gmail.com>
2022-03-11 03:05:06 +0530

    updating DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
-
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 //Get username and room
 const {username,room} = Qs.parse(location.search,{
     ignoreQueryPrefix:true
@@ -60,5 +61,11 @@ function outputMessage(message){
 
 //Add roomname to DOM
 function outputRoomName(room){
-    
+    roomName.innerText = room;
+}
+// add users to DOM 
+function outputUsers(users){
+    userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `;
 }
