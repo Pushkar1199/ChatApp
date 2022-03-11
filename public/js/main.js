08e4f92d7363b3c8499c2e2995e64b4176023926
Pushkar1199 <pushkar480@gmail.com>
2022-03-11 17:33:38 +0530

    live working

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,8 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
+
+
 //Get username and room
 const {username,room} = Qs.parse(location.search,{
     ignoreQueryPrefix:true
@@ -63,6 +65,7 @@ function outputMessage(message){
 function outputRoomName(room){
     roomName.innerText = room;
 }
+
 // add users to DOM 
 function outputUsers(users){
     userList.innerHTML = `
